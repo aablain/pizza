d185ee15b6fe28fc460c0a87b32567d3eb75a61a
Austin Blain <austin.a.blain@gmail.com>
2016-11-04 11:03:37 -0700

    calculate cost, but only with size, and uses extremely little CSS, JS for slide effect

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,29 @@ var Pizza = function(size, toppings) {
   this.toppings = toppings
 }
 
-var pizzaOne = new Pizza("small", [0])
+var pizzaOne = new Pizza("small", [])
+
+Pizza.prototype.cost = function() {
+  debugger;
+  if (this.sizes === "small") {
+    var sizeCost = 5;
+    $("#totalCostHere").text(sizeCost);
+    return sizeCost;
+  } else if (this.sizes === "medium") {
+    var sizeCost = 7;
+    $("#totalCostHere").text(sizeCost);
+    return sizeCost;
+  } else if (this.sizes === "large") {
+    var sizeCost = 9;
+    $("#totalCostHere").text(sizeCost);
+    return sizeCost;
+  }
+  // if (pizzaOne.toppings.length > 0) {
+  //   var hereWe = 2;
+  //   return hereWe;
+  // }
+}
+
 
 
 // User Interface Logic
@@ -17,17 +39,29 @@ $(document).ready(function() {
     var sizeInput = $("input:radio[name=size]:checked").val();
 
     pizzaOne.sizes = sizeInput
-    $("#sizeHere").append(pizzaOne.sizes);
+    $("#sizeHere").text(pizzaOne.sizes);
+    $("#toppingsOnPizza").delay(700).show("slide", { direction: "right" }, 1000);
+    $("#pizzaSize").hide("slide", { direction: "left" }, 1000);
   });
 
   $("form#toppingsOnPizza").submit(function(event) {
     event.preventDefault();
+    $("ul").empty();
+    pizzaOne.toppings = [];
+
 
     $("input:checkbox[name=toppings]:checked").each(function(){
       var addTopping = $(this).val();
       toppingsList.push(addTopping);
+      pizzaOne.toppings.push(addTopping);
+      alert(pizzaOne.toppings);
+      // alert(pizzaOne.toppings.length);
+      // alert(pizzaOne.toppings)
       $("#toppingsHere").append("<li>" + addTopping + "</li>");
+      pizzaOne.cost();
       // $('#work-responses').append(workTransportationMode + "<br>"); ---> Maybe for later?
     });
+    $("#review").delay(700).show("slide", { direction: "right" }, 1000);
+    $("#toppingsOnPizza").hide("slide", { direction: "left" }, 1000);
   });
 });
